9d5bf079ba9285a63ac10f1f851e7228a29bb129
William Beisley <73136073+wbeisley@users.noreply.github.com>
2021-06-28 15:26:36 +0000

    Changed variable assignments to "" as opposed to " "

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = " ";
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question= " ";
-let correctAnswer = " ";
-let candidateAnswer = " ";
+let question= "";
+let correctAnswer = "";
+let candidateAnswer = "";
 let questions = ["1) Who was the first American woman in space? ", "2) True or false: 5 Kilometer == 5000 meters. ", "3) (5+3)/2*10=? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
